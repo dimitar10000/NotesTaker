f29d5115245935af63333953553d5157ffd59413
dimitar10000 <dimitar_eco@abv.bg>
2024-03-23 12:36:41 +0200

    Fixing background and images

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,8 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <Background />
+      <body className={inter.className}>{children}
+        <Background />
+      </body>
     </html>
   );
 }
